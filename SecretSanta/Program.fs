8b0d8059ec2d84e8c91895eea3c8b4c84ff7b9bf
tr3yf1x <tr3yf1x@live.com>
2020-11-12 10:59:18 +0100

    simple website generation

--- SecretSanta/Program.fs
+++ SecretSanta/Program.fs
@@ -2,9 +2,12 @@
 open System
 open System.Text.Json
 open System.IO
+open System.Text.Encodings
+open System.Security.Cryptography
 
 type participantDetails = {Name: string;}
 type pair = {Sender:participantDetails; Receiver:participantDetails;}
+type webpublishPair = {Pair:pair; FileName:string}
 
 let participantsFile = @"C:\tmp\participants.txt"
 
@@ -34,17 +37,39 @@ let rec getValidRandomPairings allPossiblePairs participants =
     if ((random |> Seq.length) = (participants |> Seq.length)) then random
     else getValidRandomPairings allPossiblePairs participants
 
+let generateFileName (input : string) = 
+    let hashing = SHA256.Create()
+    let hashed = hashing.ComputeHash (System.Text.Encoding.Default.GetBytes input) |> Seq.map (fun c -> c.ToString("X2"))
+    String.concat "" hashed
+
+let addFileName pair =
+    {Pair=pair;FileName=(generateFileName pair.Sender.Name)}
+
+let generateWebsiteContent pair = 
+    let title = sprintf "Skandal!!! %A beschenkt ..." pair.Sender.Name
+    let stylesheetPath = @"style.css"
+    let header = sprintf "<h1>Hallo %A,</h1>" pair.Sender.Name
+    let text = sprintf "Du darfst <i>%A</i> eine schöne Kleinigkeit schenken" pair.Receiver.Name
+    let body = sprintf "%A%A" header text
+    sprintf "<!DOCTYPE html><html lang='de'><head><title>%A</title><link rel='stylesheet' href='%A'></head><body>%A</body></html>" title stylesheetPath body
+
+let buildWebsite webpublishPair = 
+    let basePath = @"C:\tmp\"
+    let filePath = sprintf "%A.%A.html" basePath webpublishPair.FileName
+
+    File.WriteAllText (filePath, (generateWebsiteContent webpublishPair.Pair)) |> ignore
+
+
 [<EntryPoint>]
 let main argv =
-    // let random = getValidRandomPairings validPairs participantDetails
-
     let participants = getParticipants participantsFile
-    let random = getValidRandomPairings (getAllValidPairs participants ) participants
-    let json = JsonSerializer.Serialize random
+    let pairings = getValidRandomPairings (getAllValidPairs participants ) participants |> Seq.map addFileName 
+    let json = pairings |> JsonSerializer.Serialize
 
     File.WriteAllText (@"C:\tmp\combined.json", json) |> ignore
 
-    printfn "%A" random
+    pairings |> Seq.map buildWebsite
+
 
 
     0
